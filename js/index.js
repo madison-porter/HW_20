b6565ef36e48853cc84cc71f4579e96713d418f9
madison-porter <mnporter12@gmail.com>
2023-02-17 11:30:52 -0800

    add additional pages

---
+++ js/index.js
@@ -0,0 +1,79 @@
+
+$("#work").on({mouseenter: function (){
+    $("#work").css({
+        "font-weight":"bold"
+    });
+},
+    mouseleave: function () {
+        $ ("#work").css ({
+            "font-weight":"normal"
+        });
+    }
+
+});
+
+$("#about").on({mouseenter: function (){
+    $("#about").css({
+        "font-weight":"bold"
+    });
+},
+    mouseleave: function () {
+        $ ("#about").css ({
+            "font-weight":"normal"
+        });
+    }
+
+});
+
+$("#resume").on({mouseenter: function (){
+    $("#resume").css({
+        "font-weight":"bold"
+    });
+},
+    mouseleave: function () {
+        $ ("#resume").css ({
+            "font-weight":"normal"
+        });
+    }
+
+});
+
+$("#linkedin").on({mouseenter: function (){
+    $("#linkedin").css({
+        "font-weight":"bold"
+    });
+},
+    mouseleave: function () {
+        $ ("#linkedin").css ({
+            "font-weight":"normal"
+        });
+    }
+
+});
+
+$(".button").on({mouseenter: function (){
+    $(".button").css({
+        "font-weight":"bold"
+    });
+},
+    mouseleave: function () {
+        $ (".button").css ({
+            "font-weight":"normal"
+        });
+    }
+
+});
+
+$("button").on({mouseenter: function (){
+    $("button").css({
+        "font-weight":"bold"
+    });
+},
+    mouseleave: function () {
+        $ ("button").css ({
+            "font-weight":"normal"
+        });
+    }
+
+});
+
